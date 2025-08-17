7324ccc9bed67839d7390eb2ba1661121d72cc2d
TanishkaChauhan <tanishka.chauhan@gmail.com>
2025-08-17 14:41:42 +0100

    Add ship speed attribute and Fix the ship going of screen bug
    the ships speed can now be changed to increase the difficulty of the game, and the ship can no longer go of the screen.

--- settings.py
+++ settings.py
@@ -7,4 +7,6 @@ class Settings:
         self.screen_width = 1200
         self.screen_height = 700
         self.bg_color = (230, 230, 230)
-        # 230, 230, 230
+
+        # ship settings
+        self.ship_speed= 1.5 # now the ship moves 1.5 pixels per cycle
--- ship.py
+++ ship.py
@@ -6,6 +6,7 @@ class Ship:
     def __init__(self,ai_game):
         """ Initalize the ship and set its staring position"""
         self.screen= ai_game.screen
+        self.settings = ai_game.settings
         self.screen_rect = ai_game.screen.get_rect()
 
         # load ship bmp into to the game and get its rectangle (hit box)
@@ -15,6 +16,9 @@ class Ship:
         # place the rectangle in which the ship is in,
         # in its starting position at the mid_bottom of the screen's rectangle
         self.rect.midbottom= self.screen_rect.midbottom
+
+        # Store a float for the ship's exact horizontal position.
+        self.x = float(self.rect.x)
 
         # Movement flag; start with a ship that's not moving.
         self.moving_right = False
@@ -26,7 +30,11 @@ class Ship:
 
     def update(self):
         """Update the ship's position based on the movement flag."""
-        if self.moving_right:
-            self.rect.x += 1
-        if self.moving_left:
-            self.rect.x -= 1
+        # the ship's x cooridinate is being updated not the rectange
+        if self.moving_right and self.rect.right < self.screen_rect.right:
+            self.x += self.settings.ship_speed
+        if self.moving_left and self.left.left < self.screen_rect.left:
+            self.x -= self.settings.ship_speed
+
+        # Update rect object from self.x.
+        self.rect.x = self.x
