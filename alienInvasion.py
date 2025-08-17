07cd1d192e0dd42d2ae847ea9518210d7b2da88a
TanishkaChauhan <tanishka.chauhan@gmail.com>
2025-08-17 15:17:49 +0100

    Added shooting bullets to ship
    there is also now a cap on how many bullets can exist on screen, and the bullets are also deleted when off screen

--- alienInvasion.py
+++ alienInvasion.py
@@ -2,6 +2,7 @@ import sys
 import pygame
 from settings import Settings
 from ship import Ship
+from bullet import Bullet
 
 
 class AlienInvasion:
@@ -19,6 +20,8 @@ class AlienInvasion:
 
     #     adding in ship
         self.ship = Ship(self)
+    #     adding in the bullets
+        self.bullets = pygame.sprite.Group()
 
     def _check_events(self):
         """Respond to keypresses and mouse events."""
@@ -38,6 +41,8 @@ class AlienInvasion:
             self.ship.moving_left = True
         elif event.key == pygame.K_q:
             sys.exit()
+        elif event.key == pygame.K_SPACE:
+            self._fire_bullets()
 
     def _check_keyup_events(self, event):
         """Respond to key releases."""
@@ -48,16 +53,36 @@ class AlienInvasion:
     def _update_screen(self):
         """Update images on the screen, and flip to the new screen."""
         self.screen.fill(self.settings.bg_color)
+        for bullet in self.bullets.sprites():
+            bullet.draw_bullet()
         self.ship.blitme()
 
         pygame.display.flip()
+
+    def _fire_bullets(self):
+        if len(self.bullets) < self.settings.bullets_allowed:
+            new_bullet = Bullet(self)
+            self.bullets.add((new_bullet))
+
+    def _update_bullets(self):
+        """Update position of bullets and get rid of old bullets."""
+        # Update bullet positions.
+        self.bullets.update()
+
+        # Get rid of bullets that have disappeared.
+        for bullet in self.bullets.copy():
+            if bullet.rect.bottom <= 0:
+                self.bullets.remove(bullet)
     def run_game(self):
         """Start the main loop for the game."""
         while True:
             self._check_events()
             self.ship.update()
+            self._update_bullets()
             self._update_screen()
             self.clock.tick(60)
+
+
 
 
 if __name__ == '__main__':
